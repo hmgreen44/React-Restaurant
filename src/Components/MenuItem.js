b71023cb2d3fabb8b9907aabc6e3375ef71c7dbf
hmgreen44 <68399145+hmgreen44@users.noreply.github.com>
2021-04-01 10:28:06 -0400

    added price, different item lists

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -6,6 +6,7 @@ function MenuItem(props) {
                 <h5 className="card-title">{props.menuItem.name}</h5>
                 <h6 className="card-subtitle mb-2 text-muted"></h6>
                 <p className="card-text">{props.menuItem.description}</p>
+                <p>{props.menuItem.price}</p>
 
             </div>
         </div>
--- src/Components/MenuSection.js
+++ src/Components/MenuSection.js
@@ -2,27 +2,53 @@ import React, { Component } from 'react'
 import MenuItem from './MenuItem'
 import axios from 'axios'
 
-class MenuSection extends React.Component {
+class MenuSection extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            menuItems: []
+            menuItems: [],
+            price: ''
         }
     }
     componentDidMount() {
-        let apiURL = 'http://awesomeincbootcampapi-ianrios529550.codeanyapp.com:3000/public/api/menu/type/'
-        let this_ = this;
         let menuItems = window.localStorage.getItem('menuItems' + this.props.id)
         if (menuItems) {
             this.setState({ menuItems: JSON.parse(menuItems) })
         }
         else {
-           
+            this.axiosHelper()
+            if (this.props.menuSection.id == 1) {
+                this.axiosHelper()
+            }
+        }
+
+    }
+    componentDidUpdate() {
+        window.localStorage.setItem('menuItems' + this.props.id, JSON.stringify(this.state.menuItems))
+    }
+
+    getPrice() {
+        let price = Math.floor((Math.random() * 10)) + 1;
+        console.log(price)
+        return price;
+
+    }
+    axiosHelper() {
+        let apiURL = 'http://awesomeincbootcampapi-ianrios529550.codeanyapp.com:3000/public/api/menu/type/'
+        let this_ = this;
         axios.get(apiURL + this.props.menuSection.id)
             .then(function (response) {
                 // handle success
                 // console.log(response.data);
-                this_.setState({ menuItems: response.data })
+                let pricedMenu = response.data.map(item => {
+                    item.price = this_.getPrice()
+                    return item;
+                })
+                if (this_.props.menuSection.id == 9) {
+                    this_.setState({ menuItems: pricedMenu.slice(0, 1) })
+                } else {
+                    this_.setState({ menuItems:this_.state.menuItems.concat(pricedMenu) })
+                }
             })
             .catch(function (error) {
                 // handle error
@@ -31,10 +57,7 @@ class MenuSection extends React.Component {
             .then(function () {
                 // always executed
             });
-        }
-    }
-    componentDidUpdate() {
-        window.localStorage.setItem('menuItems'+ this.props.id, JSON.stringify(this.state.menuItems))
+
     }
     render() {
         return (
@@ -44,7 +67,7 @@ class MenuSection extends React.Component {
                         <h2 className="card-header">{this.props.menuSection.type}</h2>
                     </div>
                 </div>
-                { this.state.menuItems.map((menuItem, index) => <MenuItem key={index} menuItem={menuItem} />)}
+                { this.state.menuItems.map((menuItem, index) => <MenuItem key={index} menuItem={menuItem} getPrice={this.getPrice} />)}
             </div>
 
         );
